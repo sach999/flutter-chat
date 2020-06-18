66e3e1795acd320feedf76ee255ff0bff016777b
Sachin RS <sachinsrs7@gmail.com>
2020-06-18 12:56:20 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:chat_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import './screens/auth_screen.dart';
+import './screens/splash_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        backgroundColor: Colors.red,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.red,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+  final Function(File pickedImage) imagePickFn;
+
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  void _pickImage() async {
+    final pickedImageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxHeight: 150,
+    );
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+    widget.imagePickFn(pickedImageFile);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text("Add image"),
+        )
+      ],
+    );
+  }
+}
